f8e5cd5a6e15132b989163ddb5dd5fbd22558714
Dmytro Rybitskyi <75dimchik57@gmail.com>
2023-11-12 15:48:26 +0200

    new 12.11 8 lr

--- лаб3/main/main.py
+++ лаб3/main/main.py
@@ -2,28 +2,45 @@ import PySimpleGUI as sg
 import my_module
 
 def main():
+    """
+    Основна функція для виконання графічного інтерфейсу.
+
+    Використовує PySimpleGUI для створення вікна з кнопками та виведенням результатів.
+    """
     sg.theme('LightBlue2')
 
+    # Опис графічного інтерфейсу
     layout = [
         [sg.Text("Початковий список чисел:")],
         [sg.Output(size=(30, 10), key='-OUTPUT-')],
-        [sg.Button("Перемішати"), sg.ColorChooserButton("Змінити колір")],
+        [sg.Button("Перемішати"), sg.Button("Видалити вивід"), sg.ColorChooserButton("Змінити колір")],
+        [sg.Text("Кількість елементів:"), sg.Text("", size=(15, 1), key='-COUNT-')],
         [sg.Button("Вийти")]
     ]
 
+    # Створення вікна
     window = sg.Window('Графічна програма', layout)
 
     while True:
         event, values = window.read()
 
+        # Перевірка подій вікна
         if event in (sg.WINDOW_CLOSED, "Вийти"):
             break
         elif event == "Перемішати":
             my_module.shuffle_and_display(window)
+        elif event == "Видалити вивід":
+            window['-OUTPUT-'].update('')
+            window['-COUNT-'].update('')
         elif event == "Змінити колір":
             color = sg.popup_get_color()
-            window['-OUTPUT-'].update("Ви вибрали колір: {}".format(color))
+            if color:
+                window['-OUTPUT-'].update("Ви вибрали колір: {}".format(color))
+        elif event == "Видалити вивід":
+            window['-OUTPUT-'].update('')
+            window['-COUNT-'].update('')
 
+    # Закриття вікна при завершенні роботи
     window.close()
 
 if __name__ == "__main__":
--- лаб3/my_module/my_module.py
+++ лаб3/my_module/my_module.py
@@ -1,11 +1,27 @@
 import random
 
 def custom_shuffle(input_list):
+    """
+    Перемішує вхідний список та повертає новий перемішаний список.
+
+    Args:
+        input_list (list): Вхідний список чисел.
+
+    Returns:
+        list: Перемішаний список чисел.
+    """
     shuffled_list = input_list.copy()
     random.shuffle(shuffled_list)
     return shuffled_list
 
 def shuffle_and_display(window):
+    """
+    Викликає функцію перемішування та відображення результатів у графічному вікні.
+
+    Args:
+        window (PySimpleGUI.Window): Графічне вікно PySimpleGUI.
+    """
     numbers = list(range(1, 11))
     shuffled_numbers = custom_shuffle(numbers)
     window['-OUTPUT-'].update("Початковий список чисел: {}\nПеремішаний список чисел: {}".format(numbers, shuffled_numbers))
+    window['-COUNT-'].update(len(shuffled_numbers))
